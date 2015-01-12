7dfe976e473db44e54359a16e452dc5fc67bda4f
Mark Heiges <mheiges@uga.edu>
2015-01-12 14:27:12 -0500

    change feeder variable names to indicate their origin

--- user-files/simulations/wdk/StrategyMix.scala
+++ user-files/simulations/wdk/StrategyMix.scala
@@ -12,11 +12,24 @@ class StrategyMix extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("EuPathDB Gatling Agent")
 
+  val httpProtocol_w1 = http
+    .baseURL("http://w1.toxodb.org/toxo")
+    .acceptHeader("text/html, */*; q=0.01")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("EuPathDB Gatling Agent")
+
+  val httpProtocol_w2 = http
+    .baseURL("http://w2.toxodb.org/toxo")
+    .acceptHeader("text/html, */*; q=0.01")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("EuPathDB Gatling Agent")
+
 
   setUp(
     GeneTextSearchScenario.scn
-      .inject(atOnceUsers(5))
+   //   .inject(atOnceUsers(5))
    //   .inject(rampUsers(50) over (5 seconds))
+       .inject(constantUsersPerSec(1) during(30 seconds))
       .protocols(httpProtocol)
   )
 }
